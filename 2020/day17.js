767edd078498c1974d5264ed758bcae6f263f477
Daniel Oakman <doakman94@gmail.com>
2020-12-25 12:36:21 +1100

    A bit of work to day17

--- 2020/day17.js
+++ 2020/day17.js
@@ -4,7 +4,7 @@ const { readFileSync } = require('fs');
 const { join } = require('path');
 
 // Gold Star:
-const map = [readFileSync(join(__dirname, 'day17-input'), { encoding: 'utf-8' })
+let map = [readFileSync(join(__dirname, 'day17-input'), { encoding: 'utf-8' })
   .split(/\s+/)
   .filter(v => v)
   .map(v => v.split(''))];
@@ -19,6 +19,14 @@ function get (map, x, y, z) {
   return map[z][y][x];
 }
 
+function set (map, x, y, z, state) {
+  if (!map[z])
+    map[z] = [];
+  if (!map[z][y])
+    map[z][y] = [];
+  map[z][y][x] = state;
+}
+
 function* neighbours (map, x, y, z) {
   for (let X = x - 1; X < x + 1; X++) {
     for (let Y = x - 1; Y < y + 1; Y++) {
@@ -30,19 +38,43 @@ function* neighbours (map, x, y, z) {
 }
 
 function* cubes (map) {
-  // for (let z = Math.min(Object.keys(get(map, 0, 0, 0))); z < get(map, 0, 0, z).length; z++) {
-  //   for (let y = Math.min(Object.keys(get(map, 0, 0, z))); y < get(map, 0, y, z).length ; y++) {
-  //     for (let x = Math.min(Object.keys(get(map, 0, y, z))); x < get(map, x, y, z).length; x++) {
-  //       yield { x, y, z, state: get(map, x, y, z) };
-  //     }
-  //   }
-  // }
-  
-}
-
-const newMap = [];
-for (const cube of cubes(map)) {
-  for (const neighbour of neighbours(map, cube.x, cube.y, cube.z)) {
-    console.log(neighbour);
+  for (let z = Math.min(...Object.keys(map)); z < map.length; z++) {
+    for (let y = Math.min(...Object.keys(map[z])); y < map[z].length ; y++) {
+      for (let x = Math.min(...Object.keys(map[z][y])); x < map[z][y].length; x++) {
+        yield { x, y, z, state: get(map, x, y, z) };
+      }
+    }
   }
 }
+
+for (let i = 0; i < 6; i++) { // 6 cycles
+  const newMap = [];
+  for (const cube of cubes(map)) {
+    const isActive = cube.state === '#';
+    let numOfActiveNeighbours = 0;
+    for (const neighbour of neighbours(map, cube.x, cube.y, cube.z)) {
+      if (neighbour.state === '#')
+        numOfActiveNeighbours++;
+      if (
+        (isActive && numOfActiveNeighbours > 3) ||
+        (!isActive && numOfActiveNeighbours > 3)
+      ) break;
+    }
+    // Set newMap:
+    set(
+      newMap, cube.x, cube.y, cube.z,
+      (
+        (isActive && (numOfActiveNeighbours === 3 || numOfActiveNeighbours === 2)) ||
+        (!isActive && (numOfActiveNeighbours === 3))
+      ) ? '#' : '.'
+    )
+  }
+  // Replace map with newMap from this cycle.
+  map = newMap;
+}
+let active = 0;
+for (const cube of cubes(map)) {
+  if (cube.state === '#')
+    active++;
+}
+console.log({ active });
