71607b5f3b62b8805239dd9f4b09f32ae2dae595
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2021-12-16 13:02:38 +1100

    Added second gold star stuff for day3 but it isn't correct

--- 2021/day3.js
+++ 2021/day3.js
@@ -29,3 +29,26 @@ sorted.forEach(([zero, one]) => {
 console.log({ powerConsumption: parseInt(gammaRate, 2) * parseInt(epsilonRate, 2) });
 
 // Gold star:
+function filterToOne (arr, predicate) {
+  for (let i = arr.length - 1; i > -1 && arr.length > 1; i--) {
+    if (!predicate(arr[i]))
+      arr.splice(i, 1);
+  }
+  return arr;
+}
+let oxy = binaryInput.slice();
+let co2 = binaryInput.slice();
+for (let i = 0; i < binaryInput[0].length; i++) {
+  if (oxy.length > 1) {
+    const oxyChar = sorted[i][0] > sorted[i][1] ? '0' : '1';
+    // oxy = oxy.filter(binary => binary[i] === oxyChar);
+    filterToOne(oxy, binary => binary[i] === oxyChar);
+  }
+  if (co2.length > 1) {
+    const co2Char = sorted[i][0] > sorted[i][1] ? '1' : '0';
+    // co2 = co2.filter(binary => binary[i] === co2Char);
+    filterToOne(co2, binary => binary[i] === co2Char);
+  }
+}
+// NOT CORRECT:
+console.log({ lifeSupportRating: parseInt(oxy[0], 2) * parseInt(co2[0], 2) });
