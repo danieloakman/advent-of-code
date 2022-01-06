415c4091ca1ecc1077f749a0b504c58c5a9948c6
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-01-06 18:45:15 +1100

    Added some stuff to day12

--- 2021/day12.js
+++ 2021/day12.js
@@ -1,9 +1,55 @@
 'use strict';
 
 const { readFileSync } = require('fs');
+const isAllCapitals = /^[A-Z]+$/;
 
-const map = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+class Node {
+  static nodes = {};
+
+  /** @private */
+  constructor (id) {
+    Node.nodes[id] = this;
+    this.id = id;
+    this.bigCave = isAllCapitals.test(id);
+    // this.visits = 0;
+    this.paths = [];
+  }
+
+  addPath (path) {
+    if (this.paths.includes(path)) return false;
+    this.paths.push(path);
+    return true;
+  }
+
+  // traverse () {
+  //   const visited = [];
+  //   const t = () => {
+
+  //   };
+  // }
+
+  static get (id) {
+    return Node.nodes[id] || new Node(id);
+  }
+
+  static traverse () {
+    const smallVisited = [];
+    const start = Node.get('start');
+    for (const path of start.paths) {
+      
+    }
+  }
+}
+
+readFileSync(__filename.replace('.js', '-input'), 'utf-8')
   .split(/[\n\r]+/)
-  .map(str => str.split('').map(parseFloat));
+  .forEach(str => {
+    const nodes = str
+      .split('-')
+      .map(id => Node.get(id));
+    nodes[0].addPath(nodes[1].id);
+    nodes[1].addPath(nodes[0].id);
+  });
 
 // First star:
+console.log(Node.traverse());
