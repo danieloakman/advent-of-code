4f8d2af6537643f0f5179c85c24385a293cbb598
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-02-18 09:31:02 +1100

    Added some days

---
+++ 2015/day11.js
@@ -0,0 +1,13 @@
+'use strict';
+// https://adventofcode.com/2015/day/11
+
+const { readFileSync } = require('fs');
+
+const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8');
+
+// First Star:
+
+
+// Second Star:
+
+
--- 2015/day9.js
+++ 2015/day9.js
@@ -5,9 +5,37 @@ const { readFileSync } = require('fs');
 
 const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
   .split(/[\n\r]+/)
+  .map(str => {
+    const [start, end, weight] = str.split(/ to | = /).map(str => str.trim());
+    return { start, end, weight: parseInt(weight) };
+  });
+
+class Graph {
+  constructor (places) {
+    this.graph = {};
+    for (const place of places) {
+      if (!this.graph[place.start])
+        this.graph[place.start] = [];
+      this.graph[place.start].push({ end: place.end, weight: place.weight });
+    }
+  }
+
+  getAllRoutes () {
+    const routes = [];
+    
+    // for (const start of Object.keys(this.graph)) {
+    //   for (const end of this.getRoutes(start)) {
+    //     routes.push(`${start} to ${end}`);
+    //   }
+    // }
+    // return routes;
+    return routes;
+  }
+}
+const graph = new Graph(input);
 
 // First Star:
-
+let a;
 
 // Second Star:
 
