111c45886c8417583b1fa17614edd56ee9a268e9
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-02-17 18:39:21 +1100

    Added some days

---
+++ 2015/day7.js
@@ -0,0 +1,22 @@
+'use strict';
+// https://adventofcode.com/2015/day/7
+
+const { readFileSync } = require('fs');
+
+const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+  .split(/[\n\r]+/)
+
+class Circuit {
+  constructor (connections) {
+    this.connections = connections;
+    this.signals = {};
+  }
+}
+
+// First Star:
+
+
+
+// Second Star:
+
+
---
+++ 2015/day8.js
@@ -0,0 +1,23 @@
+'use strict';
+// https://adventofcode.com/2015/day/8
+
+const { readFileSync } = require('fs');
+const { matches } = require('../lib/utils');
+
+const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+  .split(/[\n\r]+/)
+  .map(str => {
+    let memoryStr = str.slice(1, str.length - 1);
+    for (const match of matches(/\\x[0-9abcdef]{2}/g, memoryStr))
+      memoryStr = memoryStr.replace(match[0], String.fromCharCode(parseInt(match[0], 16)));
+    for (const match of matches(/\\[\\"]/g, memoryStr))
+      memoryStr = memoryStr.replace(match[0], match[0].slice(1));
+    return { stringCode: str.length, memoryStr: memoryStr.length };
+  });
+
+// First Star:
+console.log(input.reduce((sum, { stringCode, memoryStr }) => sum + (stringCode - memoryStr), 0));
+
+// Second Star:
+
+
