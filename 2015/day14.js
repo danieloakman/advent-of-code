53d3965d9f518736bb878123ca0d03c31c082b0c
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-02-18 13:28:10 +1100

    Finished day14

---
+++ 2015/day14.js
@@ -0,0 +1,39 @@
+'use strict';
+// https://adventofcode.com/2015/day/14
+
+const { readFileSync } = require('fs');
+const { deepCopy } = require('../lib/utils');
+
+const reindeers = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+  .split(/[\n\r]+/)
+  .map(str => {
+    const [speed, fly, rest] = str.match(/\d+/g).map(Number);
+    return { name: str.match(/^[a-z]+/i)[0], speed, fly, rest };
+  });
+
+// First and second Star:
+function race (reindeers, seconds) {
+  const distances = Object.values(reindeers)
+    .reduce((map, { name }) => {
+      map[name] = 0;
+      return map;
+    }, {});
+  const points = deepCopy(distances);
+
+  const getWinner = byScoring => Object.keys(byScoring)
+  .sort((a, b) => byScoring[b] - byScoring[a])[0];
+
+  for (let secs = 0; secs < seconds; secs++) {
+    for (const { name, speed, fly, rest } of reindeers)
+      if (secs % (fly + rest) < fly)
+        distances[name] += speed;
+    points[getWinner(distances)]++;
+  }
+
+  const distancesWinner = getWinner(distances);
+  const pointsWinner = getWinner(points);
+  console.log(`Distance winner: ${distancesWinner} with ${distances[distancesWinner]}km`);
+  console.log(`Points winner: ${pointsWinner} with ${points[pointsWinner]} points`);
+}
+
+race(reindeers, 2503);
