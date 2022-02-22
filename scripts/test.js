b2602ec549ada0878d7fd0fad144adfcdc7a08d7
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-02-22 13:15:41 +1100

    Some more work towards test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,32 @@
 'use script';
 
 const terminal = require('terminal-kit').terminal;
-const { readdirDeepSync } = require('more-node-fs');
+const { walkdir } = require('more-node-fs');
 const { join } = require('path');
+const { execSync } = require('child_process');
 
-const dirs = readdirDeepSync(join('../', __dirname))
-  .files
-  .filter(dir => /day\d+\.js/.test(dir));
+let files = [];
+for (const { path, stats } of walkdir(join(__dirname, '../')))
+  if (stats.isFile() && /day\d+\.js/.test(path))
+    files.push({ path, stats });
+files = files
+  .sort((a, b) => b.stats.mtimeMs - a.stats.mtimeMs)
+  .map(({ path }) => path.replace(process.cwd() + '\\', ''))
 
-terminal.dir
+terminal.grabInput();
+terminal.on('key', function (key, matches, data) {
+  if (key === 'CTRL_C' || key === 'ESCAPE')
+    process.exit();
+});
+
+function selectedText (err, res) {
+  // console.log(err, res);
+  try {
+    execSync(`node ${res.selectedText}`);
+  } catch (err) {
+    console.log(err);
+  }
+  terminal.gridMenu(files, selectedText);
+}
+
+terminal.gridMenu(files, selectedText);
