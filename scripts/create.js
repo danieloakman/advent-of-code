25b9ba4cc4699bc8fd2863109eaf7a5c4e0620e9
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-02-23 16:31:54 +1100

    Added semicolon

--- scripts/create.js
+++ scripts/create.js
@@ -18,7 +18,7 @@ const fileStr =
 const { readFileSync } = require('fs');
 
 const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
-  .split(/[\\n\\r]+/)
+  .split(/[\\n\\r]+/);
 
 // First Star:
 
