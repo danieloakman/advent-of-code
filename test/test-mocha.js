b25914d493914da83b573160818c1f5397538d53
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-01 12:50:32 +1100

    Added number of tests to each year in mocha title

--- test/test-mocha.js
+++ test/test-mocha.js
@@ -17,7 +17,7 @@ for (const { path, stats } of walkdir(join(__dirname, '..'))) {
 files = groupBy(files, ['year']);
 
 for (const year in files) {
-  describe(year.toString(), function () {
+  describe(`${year.toString()} - ${files[year].length}`, function () {
     for (const file of files[year].sort((a, b) => a.day - b.day)) {
       it(file.day.toString(), async function () {
         require(file.path);
