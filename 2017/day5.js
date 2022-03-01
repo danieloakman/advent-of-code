3384e85d455110a37cd8bdb5b85e93269626479d
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-02 10:17:09 +1100

    Done day5 2017

--- 2017/day5.js
+++ 2017/day5.js
@@ -4,20 +4,36 @@
 
 const { readFileSync } = require('fs');
 
-const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
-  .split(/[\n\r]+/)
-  .map(Number);
-// const input = [0, 3, 0, 1, -3];
+function getInput () {
+  // return [0, 3, 0, 1, -3];
+  return readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+    .split(/[\n\r]+/)
+    .map(Number);
+}
 
 // First Star:
-let steps = 0;
-for (let i = 0; i < input.length; steps++) {
-  const j = i;
-  i += input[i];
-  input[j]++;
+{
+  const input = getInput();
+  let steps = 0;
+  for (let i = 0; i < input.length; steps++) {
+    const j = i;
+    i += input[i];
+    input[j]++;
+  }
+  console.log({ steps });
 }
-console.log({ steps });
 
 // Second Star:
-
-
+{
+  const input = getInput();
+  let steps = 0;
+  for (let i = 0; i < input.length; steps++) {
+    const j = i;
+    i += input[i];
+    if (input[j] >= 3)
+      input[j]--;
+    else
+      input[j]++;
+  }
+  console.log({ steps });
+}
