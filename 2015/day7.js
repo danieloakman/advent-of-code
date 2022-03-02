b8b43515243ca2ccfa9112a58c93f634fe71a91e
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-02 13:23:11 +1100

    Completed day7 2015

--- 2015/day7.js
+++ 2015/day7.js
@@ -2,24 +2,57 @@
 // https://adventofcode.com/2015/day/7
 
 const { readFileSync } = require('fs');
+const once = require('lodash/once');
 
-const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
-  .split(/[\n\r]+/)
-  .map(str => {
-    const [signalA, op, ] = str.split(' ');
-  });
+function parseInput (signal, map) {
+  signal = signal.trim();
+  return once(
+    /\d+/.test(signal)
+      ? () => Number(signal)
+      : () => map[signal]()
+  );
+}
 
-class Circuit {
-  constructor (connections) {
-    this.connections = connections;
-    this.signals = {};
-  }
+function createCircuit () {
+  return readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+    .split(/[\n\r]+/)
+    .reduce((map, str) => {
+      const [input, output] = str.split('->').map(str => str.trim());
+      if (/AND/.test(input)) {
+        const [a, b] = input
+          .split(' AND ')
+          .map(signal => parseInput(signal, map));
+        map[output] = () => a() & b();
+      } else if (/OR/.test(input)) {
+        const [a, b] = input
+          .split(' OR ')
+          .map(signal => parseInput(signal, map));
+        map[output] = () => a() | b();
+      } else if (/LSHIFT/.test(input)) {
+        const [a, b] = input
+          .split(' LSHIFT ')
+          .map(signal => parseInput(signal, map));
+        map[output] = () => a() << b();
+      } else if (/RSHIFT/.test(input)) {
+        const [a, b] = input
+          .split(' RSHIFT ')
+          .map(signal => parseInput(signal, map));
+        map[output] = () => a() >> b();
+      } else if (/NOT/.test(input)) {
+        const a = parseInput(input.replace('NOT ', ''), map)
+        map[output] = () => ~a();
+      } else {
+        map[output] = parseInput(input, map);
+      }
+      return map;
+    }, {});
 }
 
 // First Star:
-const circuit = new Circuit(input);
-
+const signalA = createCircuit().a();
+console.log({ signalA });
 
 // Second Star:
-
-
+const circuit = createCircuit();
+circuit.b = () => signalA;
+console.log({ signalA: circuit.a() });
