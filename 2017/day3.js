9eab48a11095684dc0fe52c5e87dc3acc1cf215b
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-02 11:00:27 +1100

    changes t day3 2017

--- 2017/day3.js
+++ 2017/day3.js
@@ -4,22 +4,34 @@
 
 const { readFileSync } = require('fs');
 const { ok: assert } = require('assert');
-const { take } = require('../lib/utils');
+const { midNum, Map2D, fibonacci, take } = require('../lib/utils');
 
 const input = Number(readFileSync(__filename.replace('.js', '-input'), 'utf-8'));
 
 // First Star:
-// function* powers () {
-//   for (let i = 1; i; i++)
-//     yield i * i;
-// }
-// console.log(take(powers(), 10));
 function spiralDistance (num) {
   if (num < 2)
     return 0;
   for (let sideL = 1, ring = 0;; sideL += 2, ring++) {
-    if (sideL * sideL > num) { 
-      let a;
+    const volume = sideL * sideL;
+    if (volume > num) {
+      // const perimeter = (sideL * 4) - 4;
+      // const start = (sideL - 2) * (sideL - 2) + 1;
+      const corners = [
+        volume,
+        volume - (sideL - 1),
+        volume - ((sideL - 1) * 2),
+        volume - ((sideL - 1) * 3)
+      ];
+      const middles = [
+        midNum(corners[0], corners[1]),
+        midNum(corners[1], corners[2]),
+        midNum(corners[2], corners[3]),
+        midNum(corners[3], corners[3] - (sideL - 1))
+      ];
+      const distanceFromMiddle = Math.min(...middles.map(m => Math.abs(m - num)));
+      const radius = Math.floor(sideL / 2);
+      return radius + distanceFromMiddle;
     }
   }
 }
@@ -30,5 +42,32 @@ assert(spiralDistance(1024) === 31);
 console.log({ distance: spiralDistance(input) });
 
 // Second Star:
-
-
+const spiral = new Map2D();
+const directions = (function* () {
+  // [1, 1, 2, 2, 3, 3, 4, 4]
+  const directions = [
+    (x, y) => ({ x: x + 1, y }), // right
+    (x, y) => ({ x, y: y - 1 }), // up
+    (x, y) => ({ x: x - 1, y }), // left
+    (x, y) => ({ x, y: y + 1 }) // down
+  ];
+  let length = 1;
+  for (let direction = 0;; direction++) {
+    for (let i = 0; i < length; i++)
+      yield directions[direction % 4];
+    if (direction % 2)
+      length++;
+  }
+})();
+spiral.set(0, 0, 1);
+let pos = { x: 0, y: 0 };
+while (true) {
+  pos = take(directions)[0](pos.x, pos.y);
+  const sum = [...spiral.getNeighbours(pos.x, pos.y, false)]
+    .reduce((sum, { value }) => sum + value, 0);
+  if (sum > input) {
+    console.log({ sum });
+    break;
+  }
+  spiral.set(pos.x, pos.y, sum);
+}
