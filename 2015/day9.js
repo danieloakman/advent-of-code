33abbe7a6e227156e11a3f8f96610397dc63bf8a
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-03 12:01:54 +1100

    Some more work on day9

--- 2015/day9.js
+++ 2015/day9.js
@@ -3,39 +3,46 @@
 
 const { readFileSync } = require('fs');
 
-const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+const edges = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
   .split(/[\n\r]+/)
-  .map(str => {
+  .reduce((map, str) => {
     const [start, end, weight] = str.split(/ to | = /).map(str => str.trim());
-    return { start, end, weight: parseInt(weight) };
-  });
+    if (!map[start])
+      map[start] = {};
+    if (!map[end])
+      map[end] = {};
+    map[start][end] = map[end][start] = parseInt(weight);
+    return map;
+  }, {});
 
 class Graph {
-  constructor (places) {
-    this.graph = {};
-    for (const place of places) {
-      if (!this.graph[place.start])
-        this.graph[place.start] = [];
-      this.graph[place.start].push({ end: place.end, weight: place.weight });
-    }
+  constructor (edges) {
+    this.edges = edges;
+    this.places = Object.keys(edges);
   }
 
   getAllRoutes () {
     const routes = [];
-    
-    // for (const start of Object.keys(this.graph)) {
-    //   for (const end of this.getRoutes(start)) {
-    //     routes.push(`${start} to ${end}`);
-    //   }
-    // }
-    // return routes;
+    for (let start in this.edges) {
+      const route = [];
+      while (route.length < this.places.length) {
+        route.push(start);
+        start = this.getNext(route);
+        if (!start)
+          break;
+      }
+      for (const end in this.edges[start]) {
+        route.push(this.getRoute(start, end));
+      }
+      routes.push(route);
+    }
     return routes;
   }
 }
-const graph = new Graph(input);
+const graph = new Graph(edges);
 
 // First Star:
-let a;
+const routes = graph.getAllRoutes();
 
 // Second Star:
 
