31598cc4e345024e4f07ced44124106c1537e651
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-03 22:58:09 +1100

    Added unfinished day15

---
+++ 2015/day15.js
@@ -0,0 +1,36 @@
+'use strict';
+// https://adventofcode.com/2015/day/15
+// https://adventofcode.com/2015/day/15/input
+
+const { readFileSync } = require('fs');
+const { groupBy } = require('../lib/utils');
+
+const ingredients = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+  .split(/[\n\r]+/)
+  .map(str => {
+    const [capacity, durability, flavour, texture, calories] = str.match(/-?\d+/g).map(Number);
+    return {
+      name: str.match(/^[^: ]+/)[0].replace(': ', ''),
+      capacity, durability, flavour, texture, calories
+    };
+  });
+
+function* combinations (nums = 4, sumTotal = 100) {
+  // const 
+} 
+
+// First Star:
+function bestCookie (ingredients) {
+  const INGREDIENT_COUNT = 100;
+  const scores = [];
+
+  for (const ingredient of ingredients) {
+
+  }
+  return scores.sort()[0];
+}
+console.log({ score: bestCookie(ingredients) });
+
+// Second Star:
+
+
