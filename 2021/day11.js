4084f08669cb4c168fe1b39d3c815e99abfd2290
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-04 11:35:24 +1100

    Some work towards day11 2021

--- 2021/day11.js
+++ 2021/day11.js
@@ -1,70 +1,80 @@
 'use strict';
 
 const { readFileSync } = require('fs');
+const { Map2D } = require('../lib/utils');
+const { deepStrictEqual: equal } = require('assert');
 
-const map = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
-  .split(/[\n\r]+/)
-  .map(str => str.split('').map(parseFloat));
+class OctopusMap extends Map2D {
+  constructor() {
+    super();
+  }
 
-// First star:
-function get (x, y) {
-  return (map[y] && typeof map[y][x] === 'number')
-    ? map[y][x]
-    : Infinity;
-}
-function inc (x, y) {
-  if (map[y] && typeof map[y][x] === 'number')
-    map[y][x]++;
-}
-function set (x, y, value) {
-  if (map[y] && typeof map[y][x] === 'number')
-    map[y][x] = value;
-}
-function getAdjacent (x, y) {
-  return [
-    { x, y: y - 1 }, // up
-    { x, y: y + 1 }, // down
-    { x: x - 1, y }, // left
-    { x: x + 1, y }, // right
-    { x: x - 1, y: y - 1 }, // up-left
-    { x: x + 1, y: y - 1 }, // up-right
-    { x: x - 1, y: y + 1 }, // down-left
-    { x: x + 1, y: y + 1 } // down-right
-  ].filter(p => p.y >= 0 && p.y < map.length && p.x >= 0 && p.x < map[p.y].length);
-}
-function* cells () {
-  for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
-      yield { x, y };
-    }
+  inc (x, y) {
+    if (this.has(x, y))
+      this.map[`${x},${y}`]++;
+    else
+      this.map[`${x},${y}`] = 1;
   }
 }
-function simulate (steps) {
+
+function createMap (input) {
+  const map = new OctopusMap();
+  input.split(/[\n\r]+/).forEach((str, y) => {
+    const row = str.split('').map(Number);
+    for (let x = 0; x < row.length; x++)
+      map.set(x, y, row[x]);
+  });
+  return map;
+}
+const inputMap = () => createMap(readFileSync(__filename.replace('.js', '-input'), 'utf-8'));
+const testMap = () => createMap(
+`5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
+);
+
+// First star:
+/**
+ * @param {number} steps
+ * @param {OctopusMap} map
+ * @returns {number} Number of flashes.
+ */
+function simulate (steps, map) {
   let flashes = 0;
   for (let step = 0; step < steps; step++) {
-    const hasFlashed = Array.from(new Array(map.length), _ => new Array(map[0].length).fill(false));
+    const hasFlashed = new Map2D();
     function flash (x, y) {
-      if (get(x, y) > 9 && !hasFlashed[y][x]) {
-        hasFlashed[y][x] = true;
+      if (map.get(x, y) > 9 && !hasFlashed.get(x, y)) {
+        hasFlashed.set(x, y, true);
         flashes++;
-        for (const p of getAdjacent(x, y)) {
-          inc(p.x, p.y);
+        for (const p of map.getNeighbours(x, y, false)) {
+          map.inc(p.x, p.y);
           flash(p.x, p.y);
         }
       }
     }
-    for (const { x, y } of cells())
-      inc(x, y);
-    for (const { x, y } of cells())
+    for (const { x, y } of map.getValues())
+      map.inc(x, y);
+    for (const { x, y } of map.getValues())
       flash(x, y);
-    for (const { x, y } of cells())
-      if (get(x, y) > 9)
-        set(x, y, 0);
+    for (const { x, y } of map.getValues())
+      if (map.get(x, y) > 9)
+        map.set(x, y, 0);
 
-    // Second star:
-    if (hasFlashed.every(row => row.every(v => v)))
-      console.log(`Every cell has flashed on step ${step + 1}.`);
+    // if (hasFlashed.every(row => row.every(v => v)))
+    //   console.log(`Every cell has flashed on step ${step + 1}.`);
   }
   return flashes;
 }
-console.log({ flashes: simulate(1000) });
+equal(simulate(10, testMap()), 204);
+equal(simulate(100, testMap()), 1656);
+console.log({ flashes: simulate(1000, inputMap()) });
+
+// Second star:
