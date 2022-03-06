cedb11f2ff3bdc142d88262fbf81b32a9416a9a1
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-06 15:07:06 +1100

    day 14 changes

--- 2021/day14.js
+++ 2021/day14.js
@@ -1,22 +1,48 @@
 'use strict';
 
 const { readFileSync } = require('fs');
+const { stringSplice } = require('../lib/utils');
 
-const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
-  .split(/[\n\r]+/)
-const template = input[0];
-const map = input.slice(1).reduce((map, line) => {
-  const pair = line.split(' -> ');
-  map[pair[0]] = pair[1];
-  return map;
-}, {});
+const input = () => {
+  const input = readFileSync(__filename.replace('.js', '-input'), 'utf-8')
+    .split(/[\n\r]+/);
+  const template = input[0];
+  const map = input.slice(1).reduce((map, line) => {
+    const pair = line.split(' -> ');
+    map[pair[0]] = pair[1];
+    return map;
+  }, {});
+  return { template, map };
+};
+const testInput = () => {
+  const input =
+`NNCB
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`.split('\n');
+  const template = input[0];
+  const map = input.slice(1).reduce((map, line) => {
+    const pair = line.split(' -> ');
+    map[pair[0]] = pair[1];
+    return map;
+  }, {});
+  return { template, map };
+};
 
-function stringSplice (str, index, count = 1, add = '') {
-  if (index < 0 || count < 0) {
-    throw new InvalidArgumentError('index and count parameters cannot be less than zero');
-  }
-  return str.slice(0, index) + add + str.slice(index + count);
-}
+const { map, template } = testInput();
 
 function* pairs (str) {
   for (let i = str.length - 1; i > 0; i--) {
@@ -54,6 +80,9 @@ function countChars (str) {
 let chars = countChars(insertPairs(template, 10));
 console.log(chars[0][1] - chars[chars.length - 1][1]);
 
-// Second Star: TODO
+// Second Star:
+class Polymeriser {
+  constructor (template, )
+}
 chars = countChars(insertPairs(template, 40));
 console.log(chars);
