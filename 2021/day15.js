494709e3480572a64a34a2e797ef04d7c52e37ff
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-07 13:12:43 +1100

    Added some more changes to day15. Need a graph class or something to handle shortest path algorithms

--- 2021/day15.js
+++ 2021/day15.js
@@ -4,13 +4,15 @@
 
 const { readFileSync } = require('fs');
 const Map2D = require('../lib/Map2D');
+const Vector2 = require('../lib/Vector2');
+const { deepStrictEqual: equal } = require('assert');
 
 /**
  * @param {Map2D} map 
  * @param {string} line 
  * @returns {Map2D}
  */
-function parseLine (map, line) {
+function parseLine (map, line, y) {
   for (let x = 0; x < line.length; x++)
     map.set(x, y, line[x]);
   return map;
@@ -32,9 +34,29 @@ const testMap =
 `.split('\n').reduce(parseLine, new Map2D());
 
 // First Star:
+/**
+ * @param {Map2D} map
+ * @param {Vector2} start
+ * @param {Vector2} end
+ */
 function lowestRisk (map, start, end) {
+  start = start.copy;
+  end = end.copy;
+  let risk = 0;
+  const visited = { [start.toString()]: true };
+  const queue = [start];
+  while (queue) {
+    for (const neighbour of map.getNeighbours(...start)) {
 
+    }
+  }
+  return 0;
 }
+equal(
+  lowestRisk(testMap, new Vector2(0, 0), new Vector2(testMap.xMax, testMap.yMax)),
+  40
+);
+console.log({ lowestRisk: lowestRisk(map, new Vector2(), 0) });
 
 // Second Star:
 
