8c3746959b74008e89ba01bd8f4e28b11fcc01cc
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-03-08 01:24:30 +1100

    Done day5 2016

---
+++ 2016/day5.js
@@ -0,0 +1,44 @@
+'use strict';
+// https://adventofcode.com/2016/day/5
+// https://adventofcode.com/2016/day/5/input
+
+const { readFileSync } = require('fs');
+const once = require('lodash/once');
+// const { iterate } = require('iterare');
+const { deepStrictEqual: equal } = require('assert');
+const { hash, range } = require('../lib/utils');
+
+const input = once(() => readFileSync(__filename.replace('.js', '-input'), 'utf-8'));
+
+// First Star:
+function findPassword (doorID, endRange = Infinity) {
+  return range(0, endRange)
+    .map(int => hash(`${doorID}${int}`, 'md5'))
+    .filter(h => h.startsWith('00000'))
+    .map(h => h[5])
+    .take(8)
+    .join('');
+}
+// equal(findPassword('abc'), '18f47a30');
+console.log({ password: findPassword(input()) });
+
+// Second Star:
+function findPassword2 (doorID, endRange = Infinity) {
+  const password = [];
+  const positions = range(8).map(n => n.toString()).toSet();
+  for (
+    const { pos, char } of range(0, endRange, 1)
+      .map(int => hash(`${doorID}${int}`, 'md5'))
+      .filter(h => h.startsWith('00000'))
+      .map(h => ({ pos: h[5], char: h[6] }))
+      .filter(({ pos }) => positions.has(pos) && !password[pos])
+  ) {
+    password[pos] = char;
+    console.log({ password: password.join('|') });
+    if (password.join('').length === 8)
+      return password.join('');
+  }
+  return '';
+}
+// equal(findPassword2('abc'), '05ace8e3');
+console.log({ password: findPassword2(input()) });
