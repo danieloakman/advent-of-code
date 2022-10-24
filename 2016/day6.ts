05d28a3dbafd8d0b00453fff8a30a3777c273115
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-10-25 03:05:23 +1100

    Finished day6 2016

---
+++ 2016/day6.ts
@@ -0,0 +1,50 @@
+import { readFileSync } from 'fs';
+import once from 'lodash/once';
+import iter from 'iteragain/iter';
+import range from 'iteragain/range';
+import enumerate from 'iteragain/enumerate';
+import { /* ok as assert, */ deepStrictEqual as equal } from 'assert';
+import { IncrementorMap } from '../lib/IncrementorMap';
+// import * as utils from '../lib/utils';
+
+/** @see https://adventofcode.com/2016/day/6/input */
+const input = once(() => readFileSync(__filename.replace(/.[tj]s/, '-input'), 'utf-8').split(/[\n\r]+/));
+
+// https://adventofcode.com/2016/day/6 First Star:
+enum Mode {
+  LeastLikely,
+  MostLikely,
+}
+
+function decrypt(strings: string[], mode: Mode = Mode.MostLikely) {
+  const maps = iter(range(strings[0].length))
+    .map(() => new IncrementorMap())
+    .toArray();
+  for (const string of strings) for (const [index, char] of enumerate(string)) maps[index].inc(char);
+  return maps.map(map => map.minmax()[mode]).join('');
+}
+equal(
+  decrypt([
+    'eedadn',
+    'drvtee',
+    'eandsr',
+    'raavrd',
+    'atevrs',
+    'tsrnev',
+    'sdttsa',
+    'rasrtv',
+    'nssdts',
+    'ntnada',
+    'svetve',
+    'tesnvt',
+    'vntsnd',
+    'vrdear',
+    'dvrsen',
+    'enarar',
+  ]),
+  'easter',
+);
+console.log({ message: decrypt(input()) });
+
+// https://adventofcode.com/2016/day/6#part2 Second Star:
+console.log({ message: decrypt(input(), Mode.LeastLikely) });
