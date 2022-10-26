debb3ac77189d1697986ef18be587dd721b37037
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-10-26 23:06:22 +1100

    Sped up day4 2015 a little

--- years/2015/day4.ts
+++ years/2015/day4.ts
@@ -5,20 +5,22 @@ import { hash } from '../../lib/utils';
 
 const input = readFileSync(__filename.replace('.ts', '-input'), 'utf-8');
 
+let i = 0;
+
 // First Star:
-for (let i = 0; ; i++) {
+do {
   const h = hash(input + i, 'md5');
   if (h.startsWith('00000')) {
     console.log(i);
     break;
   }
-}
+} while (++i);
 
 // Second Star:
-for (let i = 0; ; i++) {
+do {
   const h = hash(input + i, 'md5');
   if (h.startsWith('000000')) {
     console.log(i);
     break;
   }
-}
+} while (++i);
