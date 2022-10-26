17585a330c9a09c5efc9af16620bc5dde1c5a348
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-10-26 23:47:45 +1100

    Refactored day4.ts

--- years/2015/day4.ts
+++ years/2015/day4.ts
@@ -1,26 +1,25 @@
 // https://adventofcode.com/2015/day/4
 
 import { readFileSync } from 'fs';
+// import iter from 'iteragain/iter';
+// import range from 'iteragain/range';
 import { hash } from '../../lib/utils';
 
 const input = readFileSync(__filename.replace('.ts', '-input'), 'utf-8');
 
-let i = 0;
+// let i = 0;
+// const zerosIt = iter(range(1, 7))
+//   .map(n => '0'.repeat(n))
+//   .cycle();
+// const zeros = zerosIt.take(6);
+const zeros = ['00000', '000000'];
 
-// First Star:
-do {
+// First & Second Star:
+for (let i = 0; ; i++) {
   const h = hash(input + i, 'md5');
-  if (h.startsWith('00000')) {
-    console.log(i);
-    break;
+  if (h.startsWith(zeros[0])) {
+    console.log(/* zeros[0],  */i);
+    zeros.shift();
+    if (!zeros.length) break;
   }
-} while (++i);
-
-// Second Star:
-do {
-  const h = hash(input + i, 'md5');
-  if (h.startsWith('000000')) {
-    console.log(i);
-    break;
-  }
-} while (++i);
+}
