ca67c58bcd98cb29a2ec70cafc6cb34f500bb2e9
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-10-31 13:16:01 +1100

    Finished day9 firstStar

--- years/2015/day9.ts
+++ years/2015/day9.ts
@@ -3,32 +3,35 @@
 import { readFileSync } from 'fs';
 import { iter } from 'iteragain';
 import once from 'lodash/once';
-import { main, multiComparator } from '../../lib/utils';
+import { main } from '../../lib/utils';
 import { deepStrictEqual as equal } from 'assert';
 
 const input = once(() => readFileSync(__filename.replace('.ts', '-input'), 'utf-8').split(/[\n\r]+/));
-const testInput1 = once(() => `London to Dublin = 464
+const testInput1 = once(() =>
+  `London to Dublin = 464
 London to Belfast = 518
-Dublin to Belfast = 141`.split(/[\n\r]+/));
-
-function parseEdge(str: string) {
-  const [start, end, weight] = str.split(/ to | = /).map(str => str.trim());
-  return [ start, end, parseFloat(weight) ] as const;
-}
+Dublin to Belfast = 141`.split(/[\n\r]+/),
+);
 
 class Graph {
   public readonly edges: Record<string, Record<string, number>> = {};
   public readonly nodes = new Set<string>();
 
-  constructor(edges?: string[]) {
+  constructor(edges?: string[], directed = false) {
     for (const edge of edges) {
-      const [start, end, weight] = parseEdge(edge);
-      this.addEdge(start, end, weight);
+      const [start, end, weight] = Graph.parseEdge(edge);
+      this.addEdge(start, end, weight, directed);
     }
   }
 
-  public addEdge(start: string, end: string, weight: number) {
+  static parseEdge(str: string) {
+    const [start, end, weight] = str.split(/ to | = /).map(str => str.trim());
+    return [start, end, parseFloat(weight)] as const;
+  }
+
+  addEdge(start: string, end: string, weight: number, directed = false) {
     this.edges[start] = Object.assign(this.edges[start] || {}, { [end]: weight });
+    if (!directed) this.edges[end] = Object.assign(this.edges[end] || {}, { [start]: weight });
     this.nodes.add(start).add(end);
   }
 
@@ -41,43 +44,11 @@ class Graph {
     return distance;
   }
 
-  public shortestEulerianPath(): number {
-    return iter(Object.keys(this.edges))
-      .map(node => this.eulerianPath(node))
-      // .tap(console.log)
-      .filter(path => path.length === this.nodes.size)
-      .tap(console.log)
-      .map(path => this.distance(path))
-      .min();
-  }
-
-  // public eulerianPath(start: string, visited = new Set<string>()): string[] {
-  //   const path = [start];
-  //   visited.add(start);
-  //   const queue: Queue<string> = [start];
-  //   while (queue.length && visited.size < this.nodes.size) {
-  //     const current = queue.shift();
-  //     if (!this.edges[current]) continue;
-  //     for (const [node, weight] of Object.entries(this.edges[current]).sort((a, b) => a[1] - b[1])) {
-  //       if (visited.has(node)) continue;
-  //       const newPaths = this.eulerianPath(node, visited);
-  //       visited.add(node);
-  //       queue.push(node);
-  //       path.push(node);
-  //     }
-  //     // const next = Object.keys(this.edges[current])
-  //     //   .filter(node => !visited.has(node))
-  //     //   .sort((a, b) => this.edges[current][a] - this.edges[current][b])?.[0];
-  //     // if (!next) break;
-  //     // visited.add(next);
-  //     // path.push(next);
-  //     // current = next;
-  //   }
-  //   if (path.length !== this.nodes.size) return [];
-  //   return path;
-  // }
-
-  public eulerianPath(start: string, visited = new Set<string>()): string[] {
+  /**
+   * Attempts to find a eulerian path starting at `start` node. Note it may not return a full eulerian path if not
+   * possible from `start`.
+   */
+  findEulerianPath(start: string, visited = new Set<string>()) {
     visited = new Set(visited.values());
     visited.add(start);
     const path = [start];
@@ -85,106 +56,45 @@ class Graph {
     const nextPaths = Object.keys(this.edges[start])
       .filter(node => !visited.has(node))
       .sort((a, b) => this.edges[start][a] - this.edges[start][b])
-      .map(node => this.eulerianPath(node, visited));
+      .map(node => this.findEulerianPath(node, visited));
     if (!nextPaths.length) return path;
-    const shortestPath = nextPaths.sort(multiComparator(
-      (a, b) => b.length - a.length,
-      (a, b) => this.distance(a) - this.distance(b),
-    ))[0];
-    return path.concat(shortestPath);
+    const pathWithMostNodes = nextPaths.sort((a, b) => b.length - a.length)[0];
+    return path.concat(pathWithMostNodes);
+  }
+
+  eulerianPaths() {
+    return iter(Object.keys(this.edges))
+      .map(node => this.findEulerianPath(node))
+      .filter(path => path.length === this.nodes.size)
+      .map(path => [path, this.distance(path)] as const)
+      .tap(console.log);
+  }
+
+  shortestEulerianPath() {
+    return this.eulerianPaths().min(([_, distance]) => distance);
+  }
+
+  longestEulerianPath() {
+    return this.eulerianPaths().max(([_, distance]) => distance);
   }
 }
 
-// class Node {
-//   static nodes: Record<string, Node> = {};
-//   // static edges: Record<string, number> = {};
-
-//   constructor(public readonly id: string, public readonly edges: Record<string, number> = {}) {
-//     Node.nodes[id] = this;
-//   }
-
-//   aStar(destId: string): number {
-//     const visited = new Set<string>();
-//     const queue: [string, number][] = [[this.id, 0 as number]];
-//     while (queue.length) {
-//       const [id, dist] = queue.shift();
-//       if (id === destId) return dist;
-//       if (visited.has(id)) continue;
-//       visited.add(id);
-//       const node = Node.nodes[id];
-//       for (const otherId in node.edges)
-//         queue.push([otherId, dist + node.edges[otherId]]);
-//     }
-//     throw new Error('No path found');
-//   }
-
-//   shortestPathVisitingAll(): number {
-//     const visited = new Set<string>();
-//     const queue: [string, number][] = [[this.id, 0 as number]];
-//     while (queue.length) {
-//       const [id, dist] = queue.shift();
-//       if (visited.has(id)) continue;
-//       visited.add(id);
-//       const node = Node.nodes[id];
-//       if (visited.size === Object.keys(Node.nodes).length) return dist;
-//       for (const otherId in node.edges)
-//         queue.push([otherId, dist + node.edges[otherId]]);
-//     }
-//     throw new Error('No path found');
-//   }
-// }
-
-// class Graph {
-//   constructor(edges) {
-//     this.edges = edges;
-//     this.places = Object.keys(edges);
-//   }
-
-//   getAllRoutes() {
-//     const routes = [];
-//     for (let start in this.edges) {
-//       const route = [];
-//       while (route.length < this.places.length) {
-//         route.push(start);
-//         start = this.getNext(route);
-//         if (!start) break;
-//       }
-//       for (const end in this.edges[start]) {
-//         route.push(this.getRoute(start, end));
-//       }
-//       routes.push(route);
-//     }
-//     return routes;
-//   }
-// }
-// const graph = new Graph(edges);
-
 // First Star:
 export async function firstStar() {
-  const graph = new Graph(input());
-  return graph.shortestEulerianPath();
+  return new Graph(input()).shortestEulerianPath();
 }
-// const routes = graph.getAllRoutes();
-
-// for (const line of input()) {
-//   const edge = parseEdge(line);
-//   const start = Node.nodes[edge.start] || new Node(edge.start);
-//   const end = Node.nodes[edge.end] || new Node(edge.end);
-//   start.edges[end.id] = edge.weight;
-// }
-// for (const node of Object.values(Node.nodes)) {
-//   console.log(node.id, node.shortestPathVisitingAll());
-// }
 
 // Second Star:
 export async function secondStar() {
-  //
+  return new Graph(input()).longestEulerianPath();
 }
 
 main(module, async () => {
   // Tests:
-  equal(new Graph(testInput1()).shortestEulerianPath(), 605);
+  equal(new Graph(testInput1()).shortestEulerianPath()[1], 605);
+  // Tests:
+  // equal(new Graph(testInput1()).longestEulerianPath()[1], 982);
 
-  console.log('First Star:', await firstStar());
-  console.log('Second Star:', await secondStar());
+  // console.log('First Star:', await firstStar());
+  // console.log('Second Star:', await secondStar());
 });
