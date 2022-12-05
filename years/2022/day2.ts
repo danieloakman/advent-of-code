2f2534b442f8f9ea94b7d637155d05e45d80f2eb
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-12-05 12:38:54 +1100

    day 2 star 2

--- years/2022/day2.ts
+++ years/2022/day2.ts
@@ -19,7 +19,7 @@ const WIN = 6;
 const DRAW = 3;
 const LOSS = 0;
 
-const CONVERT = {
+const CONVERT_STAR_1 = {
   A: RockPaperScissors.ROCK,
   B: RockPaperScissors.PAPER,
   C: RockPaperScissors.SCISSORS,
@@ -28,22 +28,20 @@ const CONVERT = {
   Z: RockPaperScissors.SCISSORS,
 };
 
-function score(me: RockPaperScissors, them: RockPaperScissors) {
-  if (me === them) return DRAW + me;
-  if (me === RockPaperScissors.ROCK && them === RockPaperScissors.SCISSORS) return WIN + me;
-  if (me === RockPaperScissors.PAPER && them === RockPaperScissors.ROCK) return WIN + me;
-  if (me === RockPaperScissors.SCISSORS && them === RockPaperScissors.PAPER) return WIN + me;
-  return LOSS + me;
-}
-
-function parseLine(line: string) {
-  const [them, me] = line.split(' ').map(char => CONVERT[char as keyof typeof CONVERT]);
-  return score(me, them);
-}
+type FirstInputChar  = 'A' | 'B' | 'C';
+type SecondInputChar = 'X' | 'Y' | 'Z';
 
 function rockPaperScissors(input: string[]) {
   return iter(input)
-    .map(parseLine)
+    .map(line => {
+      const [them, me] = line.split(' ').map(char => CONVERT_STAR_1[char as keyof typeof CONVERT_STAR_1]);
+
+      if (me === them) return DRAW + me;
+      if (me === RockPaperScissors.ROCK && them === RockPaperScissors.SCISSORS) return WIN + me;
+      if (me === RockPaperScissors.PAPER && them === RockPaperScissors.ROCK) return WIN + me;
+      if (me === RockPaperScissors.SCISSORS && them === RockPaperScissors.PAPER) return WIN + me;
+      return LOSS + me;
+    })
     // .tap(console.log)
     .reduce(sum);
 }
@@ -53,14 +51,54 @@ export async function firstStar() {
   return rockPaperScissors(input());
 }
 
+const CONVERT_STAR_2 = {
+  A: RockPaperScissors.ROCK,
+  B: RockPaperScissors.PAPER,
+  C: RockPaperScissors.SCISSORS,
+  X: {
+    // Need to lose:
+    STATE: LOSS,
+    [RockPaperScissors.ROCK]: RockPaperScissors.SCISSORS,
+    [RockPaperScissors.PAPER]: RockPaperScissors.ROCK,
+    [RockPaperScissors.SCISSORS]: RockPaperScissors.PAPER,
+  },
+  Y: {
+    // Need to draw:
+    STATE: DRAW,
+    [RockPaperScissors.ROCK]: RockPaperScissors.ROCK,
+    [RockPaperScissors.PAPER]: RockPaperScissors.PAPER,
+    [RockPaperScissors.SCISSORS]: RockPaperScissors.SCISSORS,
+  },
+  Z: {
+    // Need to win:
+    STATE: WIN,
+    [RockPaperScissors.ROCK]: RockPaperScissors.PAPER,
+    [RockPaperScissors.PAPER]: RockPaperScissors.SCISSORS,
+    [RockPaperScissors.SCISSORS]: RockPaperScissors.ROCK,
+  },
+};
+
+function rockPaperScissors2(input: string[]) {
+  return iter(input)
+    .map(line => {
+      const [them, me] = line.split(' ') as [FirstInputChar, SecondInputChar];
+      const themMove = CONVERT_STAR_2[them];
+      const meMove = CONVERT_STAR_2[me][themMove];
+      return CONVERT_STAR_2[me].STATE + meMove;
+    })
+    // .tap(console.log)
+    .reduce(sum);
+}
+
 /** @see https://adventofcode.com/2022/day/2#part2 Second Star */
 export async function secondStar() {
-  //
+  return rockPaperScissors2(input());
 }
 
 main(module, async () => {
   equal(rockPaperScissors(['A Y', 'B X', 'C Z']), 15);
-
   console.log('First star:', await firstStar());
+
+  equal(rockPaperScissors2(['A Y', 'B X', 'C Z']), 12);
   console.log('Second star:', await secondStar());
 });
