75cd0b407f08b8ee714e7ebfb41d44854ca6c757
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2022-12-06 02:23:47 +1100

    Refactored Graph into its own file

---
+++ lib/Graph.ts
@@ -0,0 +1,67 @@
+import iter from 'iteragain/iter';
+
+/** An edge representing [start, end, weight] */
+export type GraphEdgeInput = readonly [string, string, number];
+
+export class Graph {
+  public readonly edges: Record<string, Record<string, number>> = {};
+  public readonly nodes = new Set<string>();
+
+  constructor(edges?: GraphEdgeInput[], directed = false) {
+    for (const edge of edges) {
+      const [start, end, weight] = edge;
+      this.addEdge(start, end, weight, directed);
+    }
+  }
+
+  addEdge(start: string, end: string, weight: number, directed = false) {
+    this.edges[start] = Object.assign(this.edges[start] || {}, { [end]: weight });
+    if (!directed) this.edges[end] = Object.assign(this.edges[end] || {}, { [start]: weight });
+    this.nodes.add(start).add(end);
+  }
+
+  distance(nodes: string[]) {
+    let distance = 0;
+    for (const [start, end] of iter(nodes).pairwise()) {
+      if (!this.edges?.[start]?.[end]) break;
+      distance += this.edges[start][end];
+    }
+    return distance;
+  }
+
+  /**
+   * Attempts to find a eulerian path starting at `start` node. Note it may not return a full eulerian path if not
+   * possible from `start`.
+   */
+  findEulerianPath(start: string, visited = new Set<string>()) {
+    visited = new Set(visited.values());
+    visited.add(start);
+    const path = [start];
+    if (visited.size === this.nodes.size || !this.edges[start]) return path;
+    const nextPaths = Object.keys(this.edges[start])
+      .filter(node => !visited.has(node))
+      .sort((a, b) => this.edges[start][a] - this.edges[start][b])
+      .map(node => this.findEulerianPath(node, visited));
+    if (!nextPaths.length) return path;
+    const pathWithMostNodes = nextPaths.sort((a, b) => b.length - a.length)[0];
+    return path.concat(pathWithMostNodes);
+  }
+
+  eulerianPaths() {
+    return iter(Object.keys(this.edges))
+      .map(node => this.findEulerianPath(node))
+      .filter(path => path.length === this.nodes.size)
+      .map(path => [path, this.distance(path)] as const)
+      .tap(console.log);
+  }
+
+  shortestEulerianPath() {
+    return this.eulerianPaths().min(([_, distance]) => distance);
+  }
+
+  longestEulerianPath() {
+    return this.eulerianPaths().max(([_, distance]) => distance);
+  }
+}
+
+export default Graph;
--- years/2015/day9.ts
+++ years/2015/day9.ts
@@ -1,82 +1,27 @@
 // https://adventofcode.com/2015/day/9
 
-import { readFileSync } from 'fs';
-import { iter } from 'iteragain';
 import once from 'lodash/once';
 import { main } from '../../lib/utils';
 import { deepStrictEqual as equal } from 'assert';
+import { downloadInputSync } from '../../lib/downloadInput';
+import { Graph, GraphEdgeInput } from '../../lib/Graph';
 
-const input = once(() => readFileSync(__filename.replace('.ts', '-input'), 'utf-8').split(/[\n\r]+/));
+const input = once(() =>
+  downloadInputSync('2015', '9')
+    .split(/[\n\r]+/)
+    .map(parseEdge),
+);
 const testInput1 = once(() =>
   `London to Dublin = 464
 London to Belfast = 518
-Dublin to Belfast = 141`.split(/[\n\r]+/),
+Dublin to Belfast = 141`
+    .split(/[\n\r]+/)
+    .map(parseEdge),
 );
 
-class Graph {
-  public readonly edges: Record<string, Record<string, number>> = {};
-  public readonly nodes = new Set<string>();
-
-  constructor(edges?: string[], directed = false) {
-    for (const edge of edges) {
-      const [start, end, weight] = Graph.parseEdge(edge);
-      this.addEdge(start, end, weight, directed);
-    }
-  }
-
-  static parseEdge(str: string) {
-    const [start, end, weight] = str.split(/ to | = /).map(str => str.trim());
-    return [start, end, parseFloat(weight)] as const;
-  }
-
-  addEdge(start: string, end: string, weight: number, directed = false) {
-    this.edges[start] = Object.assign(this.edges[start] || {}, { [end]: weight });
-    if (!directed) this.edges[end] = Object.assign(this.edges[end] || {}, { [start]: weight });
-    this.nodes.add(start).add(end);
-  }
-
-  distance(nodes: string[]) {
-    let distance = 0;
-    for (const [start, end] of iter(nodes).pairwise()) {
-      if (!this.edges?.[start]?.[end]) break;
-      distance += this.edges[start][end];
-    }
-    return distance;
-  }
-
-  /**
-   * Attempts to find a eulerian path starting at `start` node. Note it may not return a full eulerian path if not
-   * possible from `start`.
-   */
-  findEulerianPath(start: string, visited = new Set<string>()) {
-    visited = new Set(visited.values());
-    visited.add(start);
-    const path = [start];
-    if (visited.size === this.nodes.size || !this.edges[start]) return path;
-    const nextPaths = Object.keys(this.edges[start])
-      .filter(node => !visited.has(node))
-      .sort((a, b) => this.edges[start][a] - this.edges[start][b])
-      .map(node => this.findEulerianPath(node, visited));
-    if (!nextPaths.length) return path;
-    const pathWithMostNodes = nextPaths.sort((a, b) => b.length - a.length)[0];
-    return path.concat(pathWithMostNodes);
-  }
-
-  eulerianPaths() {
-    return iter(Object.keys(this.edges))
-      .map(node => this.findEulerianPath(node))
-      .filter(path => path.length === this.nodes.size)
-      .map(path => [path, this.distance(path)] as const)
-      .tap(console.log);
-  }
-
-  shortestEulerianPath() {
-    return this.eulerianPaths().min(([_, distance]) => distance);
-  }
-
-  longestEulerianPath() {
-    return this.eulerianPaths().max(([_, distance]) => distance);
-  }
+function parseEdge(str: string): GraphEdgeInput {
+  const [start, end, weight] = str.split(/ to | = /).map(str => str.trim());
+  return [start, end, parseFloat(weight)] as const;
 }
 
 // First Star:
@@ -93,7 +38,7 @@ main(module, async () => {
   // Tests:
   equal(new Graph(testInput1()).shortestEulerianPath()[1], 605);
   // Tests:
-  // equal(new Graph(testInput1()).longestEulerianPath()[1], 982);
+  equal(new Graph(testInput1()).longestEulerianPath()[1], 982); // TODO fix
 
   // console.log('First Star:', await firstStar());
   // console.log('Second Star:', await secondStar());
