0f9d7bdd97001c71ab49cb0c721ae504c5a39f3b
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-09 11:06:56 +1000

    Improved test script

--- typescript/scripts/test.ts
+++ typescript/scripts/test.ts
@@ -1,38 +1,79 @@
-'use script';
-
 import { terminal } from 'terminal-kit';
 import { walkdirSync } from 'more-node-fs';
 import { join, relative } from 'path';
-import { execSync } from 'child_process';
+import { exec as _exec } from 'child_process';
+import { promisify } from 'util';
+const exec = promisify(_exec);
 import iter from 'iteragain/iter';
+import { ArgumentParser } from 'argparse';
+import { main } from '../lib/utils';
+import { Nullish } from '../lib/types';
 
-const files = iter(walkdirSync(join(__dirname, '../'), { ignore: /node_modules/i }))
-  .filterMap(file => file.stats.isFile() && /day\d+\.[tj]s/.test(file.path) ? file : null)
+export const files = iter(walkdirSync(join(__dirname, '../'), { ignore: /node_modules/i }))
+  .filterMap(file => (file.stats.isFile() && /day\d+\.[tj]s/.test(file.path) ? file : null))
   .toArray()
   .sort((a, b) => b.stats.mtimeMs - a.stats.mtimeMs)
   .map(({ path }) => path.replace(process.cwd() + '\\', ''));
 
 const relativeFiles = files.map(file => relative(process.cwd(), file));
 
-terminal.grabInput({});
-terminal.on('key', function (key, _matches, _data) {
-  if (key === 'CTRL_C' || key === 'ESCAPE' || key === 'q') {
-    terminal.clear('Exited test mode.');
-    process.exit();
+export async function test(year: number | string, day: number | string, { log = false }: { log?: boolean } = {}) {
+  try {
+    const result = await exec(
+      `npx ts-node --transpileOnly ${join(__dirname, '../years', year.toString(), `day${day.toString()}.ts`)}`,
+    );
+    if (log && result.stderr) terminal.error(result.stderr);
+    if (log && result.stdout) terminal(result.stdout);
+    return result.stdout;
+  } catch (e) {
+    const message = e.message ?? e;
+    if (log && message) terminal.error(message);
+    return message;
   }
-});
+}
 
-function selectedText (err, res) {
+async function selectedText(err?: Error, res?: { selectedText: string }) {
   if (err) {
     terminal.clear(err);
     process.exit(1);
   }
   try {
-    terminal.clear()(execSync(`npx ts-node --transpileOnly ${res.selectedText}`));
+    const [year, day] = res.selectedText.match(/\d+/g).map(parseFloat) as number[];
+    // terminal(`Testing day ${day} of year ${year}`);
+    terminal.clear()(await test(year, day));
   } catch (err) {
     terminal.clear()(err.stack);
   }
   terminal.gridMenu(relativeFiles, selectedText);
 }
 
-terminal.gridMenu(relativeFiles, selectedText);
+main(module, async () => {
+  const parser = new ArgumentParser({
+    description: 'Test Advent of Code Typescript solutions',
+  });
+  parser.add_argument('year', {
+    type: 'int',
+    nargs: '?',
+    default: null,
+  });
+  parser.add_argument('day', {
+    type: 'int',
+    nargs: '?',
+    default: null,
+  });
+  const args: { year: Nullish<number>; day: Nullish<number> } = parser.parse_args();
+  if (args.year && args.day) {
+    await test(args.year, args.day, { log: true });
+    return;
+  }
+
+  terminal.grabInput({});
+  terminal.on('key', function (key, _matches, _data) {
+    if (key === 'CTRL_C' || key === 'ESCAPE' || key === 'q') {
+      terminal.clear('Exited test mode.');
+      process.exit();
+    }
+  });
+
+  terminal.gridMenu(relativeFiles, selectedText);
+});
