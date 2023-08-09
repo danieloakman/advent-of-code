7e8a21375f3392b8dbef272af13056ef189a5adf
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-10 08:43:59 +1000

    Removed some commented out code

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -41,15 +41,6 @@ macro_rules! solve {
     }};
 }
 
-// pub fn read_file(folder: &str, day: u8) -> String {
-//     let cwd = env::current_dir().unwrap();
-
-//     let filepath = cwd.join("src").join(folder).join(format!("{day:02}.txt"));
-
-//     let f = fs::read_to_string(filepath);
-//     f.expect("could not open input file")
-// }
-
 pub fn read_input_file(year: u16, day: u8) -> String {
     let path = input_path(year, day);
     fs::read_to_string(path)
