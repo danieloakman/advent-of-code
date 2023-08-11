ff44dadef2f9100a680d9f435b39d691f335cd26
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-11 12:13:37 +1000

    Added Solution

--- typescript/src/lib/types.ts
+++ typescript/src/lib/types.ts
@@ -1,1 +1,6 @@
 export type Nullish<T> = T | null | undefined;
+
+export interface Solution {
+  firstStar: () => Promise<Nullish<string | number>>;
+  secondStar: () => Promise<Nullish<string | number>>;
+}
