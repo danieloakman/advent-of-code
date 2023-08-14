10f3cf8b88c20d2d877c0cc920a80f7f3997f09a
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-15 06:30:57 +1000

    Fixed up 2015 day4

--- typescript/src/years/2015/day4.ts
+++ typescript/src/years/2015/day4.ts
@@ -1,25 +1,24 @@
 // https://adventofcode.com/2015/day/4
 
-import { readFileSync } from 'fs';
-// import iter from 'iteragain/iter';
-// import range from 'iteragain/range';
-import { hash } from '../../lib/utils';
+import iter from 'iteragain/iter';
+import { hash, downloadInputSync, Solution, main } from '../../lib';
+import once from 'lodash/once';
+import count from 'iteragain/count';
 
-const input = readFileSync(__filename.replace('.ts', '-input'), 'utf-8');
+const input = once(() => downloadInputSync(2015, 4).trim());
+
+const hasher = iter(count()).map(i => [i, hash(input() + i, 'md5')] as const);
 
 // let i = 0;
 // const zerosIt = iter(range(1, 7))
 //   .map(n => '0'.repeat(n))
 //   .cycle();
 // const zeros = zerosIt.take(6);
-const zeros = ['00000', '000000'];
+const ZEROS = ['00000', '000000'];
 
-// First & Second Star:
-for (let i = 0; ; i++) {
-  const h = hash(input + i, 'md5');
-  if (h.startsWith(zeros[0])) {
-    console.log(/* zeros[0],  */i);
-    zeros.shift();
-    if (!zeros.length) break;
-  }
-}
+export const solution = new Solution(
+  once(async () => hasher.filter(([, h]) => h.startsWith(ZEROS[0])).next().value[0]),
+  once(async () => hasher.filter(([, h]) => h.startsWith(ZEROS[1])).next().value[0]),
+);
+
+main(module, () => solution.solve());
