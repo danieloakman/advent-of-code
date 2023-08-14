a58fb49aee9fd7f4aab69bfa53098c0eabcd5288
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-15 06:14:01 +1000

    day3 now uses Solution

--- typescript/src/years/2015/day3.ts
+++ typescript/src/years/2015/day3.ts
@@ -1,14 +1,13 @@
 // https://adventofcode.com/2015/day/3
 
-import { readFileSync } from 'fs';
+import { downloadInputSync, main, Solution } from '../../lib';
+import once from 'lodash/once';
 
-const input = readFileSync(__filename.replace('.ts', '-input'), 'utf-8');
+const input = once(() => downloadInputSync(2015, 3).trim());
 
 class Houses {
-  positions: Record<string, any> = {};
-  constructor() {
-    this.positions = {};
-  }
+  readonly positions: Record<string, any> = {};
+  constructor() {}
 
   inc(x: number, y: number) {
     const key = `${x},${y}`;
@@ -16,29 +15,36 @@ class Houses {
     else this.positions[key]++;
   }
 }
-// First Star:
-let santa = { x: 0, y: 0 };
-let houses = new Houses();
-for (const move of input) {
-  if (move === '^') santa.y--;
-  else if (move === 'v') santa.y++;
-  else if (move === '>') santa.x++;
-  else if (move === '<') santa.x--;
-  houses.inc(santa.x, santa.y);
-}
-console.log({ houses: Object.keys(houses.positions).length });
 
-// Second Star:
-santa = { x: 0, y: 0 };
-const roboSanta = { x: 0, y: 0 };
-houses = new Houses();
-for (let i = 0; i < input.length; i++) {
-  const move = input[i];
-  const s = i % 2 === 0 ? santa : roboSanta;
-  if (move === '^') s.y--;
-  else if (move === 'v') s.y++;
-  else if (move === '>') s.x++;
-  else if (move === '<') s.x--;
-  houses.inc(s.x, s.y);
-}
-console.log({ houses: Object.keys(houses.positions).length });
+export const solution = new Solution(
+  async function firstStar() {
+    const santa = { x: 0, y: 0 };
+    const houses = new Houses();
+    for (const move of input()) {
+      if (move === '^') santa.y--;
+      else if (move === 'v') santa.y++;
+      else if (move === '>') santa.x++;
+      else if (move === '<') santa.x--;
+      houses.inc(santa.x, santa.y);
+    }
+    // console.log({ houses: Object.keys(houses.positions).length });
+    return Object.keys(houses.positions).length;
+  },
+  async function secondStar() {
+    const santa = { x: 0, y: 0 };
+    const roboSanta = { x: 0, y: 0 };
+    const houses = new Houses();
+    for (let i = 0; i < input().length; i++) {
+      const move = input()[i];
+      const s = i % 2 === 0 ? santa : roboSanta;
+      if (move === '^') s.y--;
+      else if (move === 'v') s.y++;
+      else if (move === '>') s.x++;
+      else if (move === '<') s.x--;
+      houses.inc(s.x, s.y);
+    }
+    return Object.keys(houses.positions).length;
+  }
+);
+
+main(module, () => solution.solve());
