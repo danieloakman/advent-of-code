2b94a85384f4cf71b14abf676d2d027d992759d8
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-14 14:58:03 +1000

    Updated 2015/day1.ts

--- typescript/src/years/2015/day1.ts
+++ typescript/src/years/2015/day1.ts
@@ -1,10 +1,29 @@
 // https://adventofcode.com/2015/day/1
 
 import once from 'lodash/once';
-import { downloadInput } from '../../lib/downloadInput';
-import { main } from '../../lib/utils';
+import { main, downloadInput, Solution } from '../../lib';
 
 const input = once(() => downloadInput('2015', '1'));
+
+async function solve() {
+  let floor = 0;
+  let firstBasement = null;
+  let charNum = 0;
+
+  for (const char of await input()) {
+    charNum++;
+    if (char === '(') floor++;
+    else floor--;
+    if (floor === -1 && firstBasement === null) firstBasement = charNum;
+  }
+
+  return { floor, firstBasement };
+}
+
+export const solution: Solution = {
+  firstStar: () => solve().then(({ floor }) => floor),
+  secondStar: () => solve().then(({ firstBasement }) => firstBasement),
+};
 
 main(module, async () => {
   // First Star:
