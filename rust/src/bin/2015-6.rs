783540fabead6d76f5606de3f80d0690f78cebfc
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-15 06:09:01 +1000

    Added unfinished 2015-6.rs

---
+++ rust/src/bin/2015-6.rs
@@ -0,0 +1,28 @@
+// Input: https://adventofcode.com/2015/day/6/input
+
+/// See [2015/6](https://adventofcode.com/2015/day/6)
+pub fn part_one(_input: &str) -> Option<i64> {
+    None
+}
+
+/// See [2015/6](https://adventofcode.com/2015/day/6#part2)
+pub fn part_two(_input: &str) -> Option<i64> {
+    None
+}
+
+fn main() {
+    let input = &aoc::get_input(2015, 6);
+    aoc::solve!(1, part_one, input);
+    aoc::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests_2015_6 {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_part_one() {
+        unimplemented!("");
+    }
+}
