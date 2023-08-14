a35ac8cb45947f3c222432163bbd866c36b4dfb4
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-14 14:38:20 +1000

    More work towards part 2

--- rust/src/bin/2015-5.rs
+++ rust/src/bin/2015-5.rs
@@ -3,7 +3,7 @@
 #[macro_use]
 extern crate lazy_static;
 use aoc::helpers::*;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 lazy_static! {
     static ref NOT_ALLOWED: HashSet<&'static str> = HashSet::from(["ab", "cd", "pq", "xy"]);
@@ -40,8 +40,34 @@ fn is_nice(str: &str) -> bool {
     false
 }
 
-fn is_nice_2(_input: &str) -> bool {
-    todo!("Implement is_nice_2");
+fn is_nice_2(input: &str) -> bool {
+    // let bytes = input.as_bytes().to_owned();
+    let mut map: HashMap<&str, usize>  = HashMap::new();
+    let mut contains_two_pairs = false;
+    let mut contains_repeat = false;
+
+    for i in 0..input.len() {
+        if i < input.len() - 1 && !contains_two_pairs {
+            let pair = &input[i..i + 2];
+            let value = map.get(pair);
+            if value.is_none() {
+                map.insert(pair, 1);
+            } else if value.unwrap() != &i && value.unwrap() != &(i + 1) {
+                contains_two_pairs = true;
+            }
+        }
+        if i < input.len() - 2 && !contains_repeat {
+            let pair = &input[i..i + 3];
+            if char_at(pair, 0) == char_at(pair, 2) {
+                contains_repeat = true;
+            }
+        }
+        if contains_two_pairs && contains_repeat {
+            return true;
+        }
+    }
+
+    contains_two_pairs && contains_repeat
 }
 
 /// See [2015/5](https://adventofcode.com/2015/day/5)
@@ -55,13 +81,14 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 /// See [2015/5](https://adventofcode.com/2015/day/5#part2)
-pub fn part_two(input: &str) -> Option<u64> {
-    Some(it_len(
-        input
-            .split('\n')
-            .filter(|s| !s.is_empty())
-            .filter(|s| is_nice_2(s)))
-    )
+pub fn part_two(_input: &str) -> Option<u64> {
+    todo!("Part 2 answer is wrong");
+    // Some(it_len(
+    //     input
+    //         .split('\n')
+    //         .filter(|s| !s.is_empty())
+    //         .filter(|s| is_nice_2(s)))
+    // )
 }
 
 fn main() {
