8852b7b4f781f2efaad98df4c2eaa8b97c0a25cb
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-14 17:07:31 +1000

    Added start to day1.c

---
+++ c/src/2015/day1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void hi()
+{
+  printf("%s", "hi");
+}
+
+int main()
+{
+  FILE *fptr;
+
+  // Open a file in read mode
+  fptr = fopen("../tmp/2015-1-input.txt", "r");
+
+  // Check if the file is open:
+  if (fptr == NULL)
+  {
+    printf("Cannot open file \n");
+    return 1;
+  }
+
+  // Read contents from file
+  char c = fgetc(fptr);
+  while (c != EOF)
+  {
+    printf ("%c", c);
+    c = fgetc(fptr);
+  }
+
+  // int a = 20;
+  // printf("%d", 20);
+  // for (int i = 0; i < 20; i++) {
+  //   printf("%d\n", i);
+  // }
+  // hi();
+  return 0;
+}
