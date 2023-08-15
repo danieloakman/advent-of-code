861538fd1fcff5aa760c8de863f59ec290ef284e
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-15 15:16:58 +1000

    2015/day6.ts now uses Solution

--- typescript/src/years/2015/day6.ts
+++ typescript/src/years/2015/day6.ts
@@ -1,8 +1,11 @@
 // https://adventofcode.com/2015/day/6
 
-import { readFileSync } from 'fs';
+import { Solution, downloadInputSync, main } from '../../lib';
+import once from 'lodash/once';
 
-const input = readFileSync(__filename.replace('.ts', '-input'), 'utf-8')
+type Point = [x: number, y: number];
+
+const input = once(() =>downloadInputSync(2015, 6)
   .split(/[\n\r]+/)
   .map(str => {
     const cmd = str.match(/^\D+/)[0].trim();
@@ -10,40 +13,45 @@ const input = readFileSync(__filename.replace('.ts', '-input'), 'utf-8')
       .replace(cmd, '')
       .trim()
       .split(' through ')
-      .map(nums => nums.split(',').map(Number));
+      .map(nums => nums.split(',').map(Number) as Point);
     return { cmd, start, end };
-  });
+  }));
 
-function* lightsWithin(start, end) {
+function* lightsWithin(start: Point, end: Point) {
   for (let x = start[0]; x <= end[0]; x++) {
     for (let y = start[1]; y <= end[1]; y++) {
-      yield { x, y };
+      yield [x, y] as Point;
     }
   }
 }
 
-function countLights(lights) {
+function countLights(lights: number[][]) {
   return lights.reduce((acc, row) => acc + row.reduce((acc, light) => acc + light, 0), 0);
 }
 
-// First Star:
-let lights = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
-for (const { cmd, start, end } of input) {
-  for (const { x, y } of lightsWithin(start, end)) {
-    if (cmd === 'turn on') lights[y][x] = 1;
-    else if (cmd === 'turn off') lights[y][x] = 0;
-    else if (cmd === 'toggle') lights[y][x] = lights[y][x] === 0 ? 1 : 0;
-  }
-}
-console.log({ numOfLightsTurnedOn: countLights(lights) });
+export const solution = new Solution(
+  async () => {
+    const lights = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
+    for (const { cmd, start, end } of input()) {
+      for (const [x, y] of lightsWithin(start, end)) {
+        if (cmd === 'turn on') lights[y][x] = 1;
+        else if (cmd === 'turn off') lights[y][x] = 0;
+        else if (cmd === 'toggle') lights[y][x] = lights[y][x] === 0 ? 1 : 0;
+      }
+    }
+    return countLights(lights);
+  },
+  async () => {
+    const lights = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
+    for (const { cmd, start, end } of input()) {
+      for (const [x, y] of lightsWithin(start, end)) {
+        if (cmd === 'turn on') lights[y][x] += 1;
+        else if (cmd === 'turn off') lights[y][x] = Math.max(lights[y][x] - 1, 0);
+        else if (cmd === 'toggle') lights[y][x] += 2;
+      }
+    }
+    return countLights(lights);
+  },
+);
 
-// Second Star:
-lights = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
-for (const { cmd, start, end } of input) {
-  for (const { x, y } of lightsWithin(start, end)) {
-    if (cmd === 'turn on') lights[y][x] += 1;
-    else if (cmd === 'turn off') lights[y][x] = Math.max(lights[y][x] - 1, 0);
-    else if (cmd === 'toggle') lights[y][x] += 2;
-  }
-}
-console.log({ numOfLightsTurnedOn: countLights(lights) });
+main(module, () => solution.solve());
