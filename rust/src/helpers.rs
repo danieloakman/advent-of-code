6371312357cedd6058dfdbfa5cfe06d343be516d
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-15 16:42:11 +1000

    helpers.rs now has unit tests

--- rust/src/helpers.rs
+++ rust/src/helpers.rs
@@ -13,23 +13,6 @@
 //     closure
 // }
 
-// #[cfg(test)]
-// mod tests_helpers {
-//     use super::*;
-
-//     #[test]
-//     fn test_once() {
-//         let mut counter = 0;
-//         let f = once(|| {
-//             counter += 1;
-//             counter
-//         });
-//         f();
-//         // once(|| counter += 1);
-//         assert_eq!(counter, 1);
-//     }
-// }
-
 pub fn it_len<T>(iter: impl Iterator<Item = T>) -> u64 {
     iter.fold(0, |acc, _| acc + 1)
 }
@@ -55,11 +38,13 @@ pub fn char_at(string: &str, index: usize) -> Option<&str> {
 /// 
 /// #### Example
 /// ```rust
+/// use aoc::map;
+/// use std::collections::HashMap;
 /// let mut map1 = map!(String, u64);
 /// let mut map2 = map!(
 ///     "id1".to_owned() => 0,
-///     "id2".to_owned() => 1,
-/// )
+///     "id2".to_owned() => 1
+/// );
 /// ```
 #[macro_export]
 macro_rules! map {
@@ -78,4 +63,44 @@ macro_rules! map {
             map
         }
     };
+}
+
+#[cfg(test)]
+mod tests_helpers {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_it_len() {
+        assert_eq!(it_len(vec![1, 2, 3].into_iter()), 3);
+        assert_eq!(it_len(vec![1, 2, 3, 4].into_iter()), 4);
+    }
+
+    #[test]
+    fn test_pairwise() {
+        let mut iter = pairwise(vec![1, 2, 3, 4].into_iter());
+        assert_eq!(iter.next(), Some((1, 2)));
+        assert_eq!(iter.next(), Some((2, 3)));
+        assert_eq!(iter.next(), Some((3, 4)));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn test_char_at() {
+        assert_eq!(char_at("abc", 0), Some("a"));
+        assert_eq!(char_at("abc", 1), Some("b"));
+        assert_eq!(char_at("abc", 2), Some("c"));
+        assert_eq!(char_at("abc", 3), None);
+    }
+
+    #[test]
+    fn test_map() {
+        let map1 = map!(String, u64);
+        let map2 = map!(
+            "id1" => 0,
+            "id2" => 1
+        );
+        assert_eq!(map1.len(), 0);
+        assert_eq!(map2.len(), 2);
+    }
 }
