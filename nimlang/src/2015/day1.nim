d31442041de3115cf84c155c3f7d81b8b2b1341f
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-17 17:41:16 +1000

    More work on day1.nim

--- nimlang/src/2015/day1.nim
+++ nimlang/src/2015/day1.nim
@@ -1,11 +1,26 @@
-proc solve(): int =
-  let f = open("../tmp/2015-1-input.txt")
-  # Close the file object when you are done with it
-  defer: f.close()
+import std/strutils
 
+iterator fileLines(path: string): string =
+  let f = open(path)
+  defer: f.close()
   while not f.endOfFile:
-    let line = f.readLine()
-    echo line
+    yield f.readLine()
+
+proc solve(): (int, int) =
+  var
+    floor = 0
+    firstBasement = -1
+    charNum = 0
+
+  for line in fileLines("../tmp/2015-1-input.txt"):
+    for char in line:
+      charNum += 1
+      if char == '(': floor += 1
+      elif char == ')': floor -= 1
+      if floor == -1 and firstBasement == -1:
+        firstBasement = charNum
+        
+  return (floor, firstBasement)
 
 when isMainModule:
   discard solve()
