54133142d5280bbde2a51966d0f089fefba885e3
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-17 17:28:50 +1000

    Now prints lines

--- nimlang/src/2015/day1.nim
+++ nimlang/src/2015/day1.nim
@@ -1,12 +1,11 @@
 proc solve(): int =
-  let f = open("../")
+  let f = open("../tmp/2015-1-input.txt")
   # Close the file object when you are done with it
   defer: f.close()
 
-  while f.endOfFile:
+  while not f.endOfFile:
     let line = f.readLine()
     echo line
-    
 
 when isMainModule:
   discard solve()
