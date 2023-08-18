2a719ae6035296b1f34d773f832fb00a61c6d618
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-08-18 16:38:17 +1000

    2016/day1.ts uses Solution

--- typescript/src/years/2016/day1.ts
+++ typescript/src/years/2016/day1.ts
@@ -1,38 +1,59 @@
 // https://adventofcode.com/2016/day/1
+import { Solution, canTest } from '../../lib';
+import memoize from 'lodash/memoize';
+import { describe, expect, it } from 'vitest';
 
-import { readFileSync } from 'fs';
+type Point = { x: number; y: number };
 
-const input = readFileSync(__filename.replace('.ts', '-input'), 'utf-8').split(/, +/);
+const solve = memoize((input: string) => {
+  const pos: Point = { x: 0, y: 0 };
+  let blocksAwayFromFirstPosVisitedTwice = null;
+  const positions = new Set<Point>();
+  let facing = 'N';
+  for (const cmd of input.split(', ')) {
+    const turn = cmd[0];
+    const dist = parseInt(cmd.slice(1), 10);
+    if (turn === 'L') {
+      facing = { N: 'W', W: 'S', S: 'E', E: 'N' }[facing];
+    } else {
+      facing = { N: 'E', E: 'S', S: 'W', W: 'N' }[facing];
+    }
+    if (facing === 'N') {
+      pos.y -= dist;
+    } else if (facing === 'S') {
+      pos.y += dist;
+    } else if (facing === 'E') {
+      pos.x += dist;
+    } else if (facing === 'W') {
+      pos.x -= dist;
+    }
 
-// First Star:
-const pos = { x: 0, y: 0 };
-let blocksAwayFromFirstPosVisitedTwice = null;
-const positions = {};
-let facing = 'N';
-for (const cmd of input) {
-  const turn = cmd[0];
-  const dist = parseInt(cmd.slice(1), 10);
-  if (turn === 'L') {
-    facing = { N: 'W', W: 'S', S: 'E', E: 'N' }[facing];
-  } else {
-    facing = { N: 'E', E: 'S', S: 'W', W: 'N' }[facing];
+    if (!blocksAwayFromFirstPosVisitedTwice) {
+      if (positions.has(pos))
+        blocksAwayFromFirstPosVisitedTwice = dist;
+      else positions.add(pos);
+    }
   }
-  if (facing === 'N') {
-    pos.y += dist;
-  } else if (facing === 'S') {
-    pos.y -= dist;
-  } else if (facing === 'E') {
-    pos.x += dist;
-  } else if (facing === 'W') {
-    pos.x -= dist;
-  }
+  return { blocksAway: Math.abs(pos.x) + Math.abs(pos.y), blocksAwayFromFirstPosVisitedTwice };
+});
 
-  const key = `${pos.x},${pos.y}`;
-  if (!blocksAwayFromFirstPosVisitedTwice && positions[key])
-    blocksAwayFromFirstPosVisitedTwice = Math.abs(pos.x) + Math.abs(pos.y);
-  else if (!blocksAwayFromFirstPosVisitedTwice) positions[key] = 1;
+export const solution = new Solution(
+  2016,
+  1,
+  async input => solve(input).blocksAway,
+  async input => solve(input).blocksAwayFromFirstPosVisitedTwice,
+);
+
+solution.main(module);
+
+// import.meta.vitest
+if (canTest()) {
+  describe('2016/1', () => {
+    it('examples', async () => {
+      expect(solve('R2, L3').blocksAway).toEqual(5);
+      expect(solve('R2, R2, R2').blocksAway).toEqual(2);
+      expect(solve('R5, L5, R5, R3').blocksAway).toEqual(12);
+      expect(solve('R8, R4, R4, R8').blocksAwayFromFirstPosVisitedTwice).toEqual(4);
+    });
+  });
 }
-console.log({ blocksAway: Math.abs(pos.x) + Math.abs(pos.y) });
-
-// Second Star:
-console.log({ blocksAwayFromFirstPosVisitedTwice });
