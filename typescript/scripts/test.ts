e12fa1eea561633a9ea08cc1eab8593aa5405cad
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-09-15 07:18:09 +1000

    Fixed replace bug

--- typescript/scripts/test.ts
+++ typescript/scripts/test.ts
@@ -2,7 +2,7 @@ import { main, sh, parseArgs } from '@lib';
 import { globIterate } from 'glob';
 
 main(import.meta.path, async () => {
-  const args: { path: string, watch: boolean } = parseArgs({ description: 'Run tests, either "in source" or `.test` files.' }, ['path', { help: 'Either a path or glob' }], [
+  const args: { path: string; watch: boolean } = parseArgs({ description: 'Run tests, either "in source" or `.test` files.' }, ['path', { help: 'Either a path or glob' }], [
     '--watch',
     '-w',
     { action: 'store_true', default: false },
