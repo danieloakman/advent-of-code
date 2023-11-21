5c88a53ead919f1cc547a46e47b508ee83579109
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-11-22 06:34:33 +1100

    Converted 2015 day9

--- typescript/src/years/2015/day9.ts
+++ typescript/src/years/2015/day9.ts
@@ -1,21 +1,14 @@
 // https://adventofcode.com/2015/day/9
 
 import once from 'lodash/once';
-import { main } from '../../lib/utils';
 import { deepStrictEqual as equal } from 'assert';
-import { downloadInputSync } from '../../lib/downloadInput';
-import { Graph, GraphEdgeInput } from '../../lib/Graph';
+import { Solution, Graph, GraphEdgeInput, newLine } from '@lib';
 
-const input = once(() =>
-  downloadInputSync('2015', '9')
-    .split(/[\n\r]+/)
-    .map(parseEdge),
-);
 const testInput1 = once(() =>
   `London to Dublin = 464
 London to Belfast = 518
 Dublin to Belfast = 141`
-    .split(/[\n\r]+/)
+    .split(newLine)
     .map(parseEdge),
 );
 
@@ -24,22 +17,11 @@ function parseEdge(str: string): GraphEdgeInput {
   return [start, end, parseFloat(weight)] as const;
 }
 
-// First Star:
-export async function firstStar() {
-  return new Graph(input()).shortestEulerianPath();
-}
-
-// Second Star:
-export async function secondStar() {
-  return new Graph(input()).longestEulerianPath();
-}
-
-main(module, async () => {
-  // Tests:
-  equal(new Graph(testInput1()).shortestEulerianPath()[1], 605);
-  // Tests:
-  equal(new Graph(testInput1()).longestEulerianPath()[1], 982); // TODO fix
-
-  // console.log('First Star:', await firstStar());
-  // console.log('Second Star:', await secondStar());
-});
+export const solution = new Solution(2015, 9)
+  .firstStar(async input => new Graph(input.split(newLine).map(parseEdge)).shortestEulerianPath()[1])
+  .secondStar(async input => new Graph(input.split(newLine).map(parseEdge)).longestEulerianPath()[1])
+  .test.todo('examples', () => {
+    equal(new Graph(testInput1()).shortestEulerianPath()[1], 605);
+    equal(new Graph(testInput1()).longestEulerianPath()[1], 982); // TODO fix
+  })
+  .main(import.meta.path);
