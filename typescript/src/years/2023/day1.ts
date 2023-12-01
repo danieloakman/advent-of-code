f763022204c3e7937e7d190dda7a70541c34516f
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-12-01 17:36:01 +1100

    Added start to 2023 1

---
+++ typescript/src/years/2023/day1.ts
@@ -0,0 +1,29 @@
+// @see https://adventofcode.com/2023/day/1/input
+import { Solution, add, newLine } from '@lib';
+import { expect } from 'bun:test';
+import { iter } from 'iteragain-es';
+
+function isNumber(str: string): boolean {
+  return !isNaN(Number(str));
+}
+
+function parseLine(str: string): number {
+  const [first] = iter(str).filter(isNumber).take(1);
+  const [last] = iter(str).reverse().filter(isNumber).take(1);
+  return parseInt(first + last);
+}
+
+function parseLines(lines: string[]): number {
+  return iter(lines).map(parseLine).reduce(add);
+}
+
+export const solution = new Solution(2023, 1)
+  .firstStar(async input => parseLines(input.split(newLine)))
+  // .secondStar(async input => null)
+  .test('example', async () => {
+    expect(parseLines(`1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet`.split(newLine))).toEqual(142);
+  })
+  .main(import.meta.path);
