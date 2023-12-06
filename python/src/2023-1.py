85b0ca5c54b63ec760bfb3d22b328c72e6dd7ead
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-12-06 17:19:14 +1100

    Added 2023-1.py

---
+++ python/src/2023-1.py
@@ -0,0 +1,40 @@
+from lib.misc import file_input, txt
+import itertools
+
+def is_number(s: str) -> bool:
+  try:
+    int(s)
+    return True
+  except ValueError:
+    return False
+
+def parse_line(s: str) -> int:
+  nums = list(filter(is_number, s))
+  return int(nums[0] + nums[-1])
+
+def first_star(input: str) -> int:
+  return sum(map(parse_line, input.splitlines()))
+  # for line in file_input_lines(2023, 1):
+    # print(line)
+
+def second_star(input: str) -> int:
+  return 0
+
+if __name__ == "__main__":
+    assert first_star(txt("""
+      1abc2
+      pqr3stu8vwx
+      a1b2c3d4e5f
+      treb7uchet
+    """)) == 142
+    print(first_star(file_input(2023, 1)))
+    # assert second_star(txt("""
+    #   two1nine
+    #   eightwothree
+    #   abcone2threexyz
+    #   xtwone3four
+    #   4nineeightseven2
+    #   zoneight234
+    #   7pqrstsixteen
+    # """)) == 281
+    # print(second_star(file_input(2023, 1)))
---
+++ python/src/lib/misc.py
@@ -0,0 +1,25 @@
+import os
+from functools import reduce
+
+def file_input_path(year: int, day: int) -> str:
+  return os.path.join(os.getcwd(), '../tmp', "{}-{}-input.txt".format(year, day))
+
+def file_input(year: int, day: int) -> str:
+    """Return the contents of the input file for the given year and day."""
+    path = file_input_path(year, day)
+    with open(path) as f:
+        return f.read()
+
+def file_input_lines(year: int, day: int) -> list:
+    """Return the contents of the input file for the given year and day as a list of lines."""
+    path = file_input_path(year, day)
+    with open(path, "r") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            yield line.strip()
+
+def txt(input: str) -> str:
+    """Trims the leading whitespace from every line in the given string, and trims the start and end of the whole string."""
+    return reduce(lambda a, b: a + "\n" + b, map(lambda s: s.lstrip(), input.splitlines())).strip()
