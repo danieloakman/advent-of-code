2751b852d181c39f91433b2d28895203e6fa4ac2
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-12-07 16:36:37 +1100

    Finished python 2023 day 2

--- python/src/2023_2.py
+++ python/src/2023_2.py
@@ -49,18 +49,48 @@ def first_star(string: str) -> int:
     return sum(map(lambda g: g.id, possible_games))
 
 
+def min_possible_pick(game: Game) -> Pick:
+    result = [0, 0, 0]
+    for pick in game.picks:
+        for i in range(3):
+            if pick[i] > result[i]:
+                result[i] = pick[i]
+    return tuple(result)
+
+
+def second_star(string: str) -> int:
+    return sum(map(lambda x: x[0] * x[1] * x[2], map(min_possible_pick, map(parse_game, string.splitlines()))))
+
+
 if __name__ == "__main__":
-    # assert (
-    #     first_star(
-    #         txt(
-    #             """
-    #     Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-    #     Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-    #     Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-    #     Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-    #     Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
-    #         )
-    #     )
-    #     == 8
-    # )
+    assert (
+        first_star(
+            txt(
+                """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+            )
+        )
+        == 8
+    )
     print(first_star(file_input(2023, 2)))
+
+    assert (
+        (
+            second_star(
+                txt(
+                    """
+        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+                )
+            )
+        )
+        == 2286
+    )
+    print(second_star(file_input(2023, 2)))
