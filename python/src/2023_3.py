2b1f0b55293c5915666188f29fb1d520062ab206
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-12-08 14:01:41 +1100

    Solved day3 first star

--- python/src/2023_3.py
+++ python/src/2023_3.py
@@ -1,6 +1,8 @@
 import numpy as np
+from typing import Generator
 from lib.misc import assert_equal, txt, safe_int, string_dimensions
 from lib.puzzle_input import puzzle_input
+
 
 def parse_ndarray(text: str):
     arr = np.ndarray(shape=string_dimensions(text), dtype=int)
@@ -16,22 +18,46 @@ def parse_ndarray(text: str):
     return arr
 
 
+Point = tuple[int, int]
+
+
+def neighbours(p: Point, arr: np.ndarray) -> Generator[tuple[Point, int], None, None]:
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if x == 0 and y == 0:
+                continue
+            n_x = p[0] + x
+            n_y = p[1] + y
+            if n_x < 0 or n_y < 0 or n_x >= arr.shape[0] or n_y >= arr.shape[1]:
+                continue
+            yield ((n_x, n_y), arr[n_x][n_y])
+
+
 def first_star(text: str) -> int:
     """Solution to the 1st star of the day"""
     arr = parse_ndarray(text)
-    for x in arr:
-        for y in x:
-            print(y, end="")
-        print()
-    # (x_len, y_len) = arr.shape
-    # for x in range(x_len):
-    #     for y in range(y_len):
-    #         print(arr[x][y], end="")
-    #     print()
-    # for i, line in enumerate(text.splitlines()):
-    #     for j, char in enumerate(line):
-    #         arr[i][j] = 1 if char == "#" else 0
-    return 0
+
+    part_nums: list[int] = []
+    for x, row in enumerate(arr):
+        last_part_num = ""
+        is_part_num = False
+        for y, n in enumerate(row):
+            if n > 9:
+                if is_part_num:
+                    part_nums.append(int(last_part_num))
+                    is_part_num = False
+                last_part_num = ""
+                continue
+            last_part_num += str(n)
+            if not is_part_num:
+                for _p, n in neighbours((x, y), arr):
+                    if n == 11:
+                        is_part_num = True
+                        break
+        if is_part_num:
+            part_nums.append(int(last_part_num))
+
+    return sum(part_nums)
 
 
 if __name__ == "__main__":
