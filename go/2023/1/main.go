59acfbdffd6feb987b8e63402248b79a56158c0d
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-01-04 07:56:40 +1100

    More golang stuff

---
+++ go/2023/1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"danieloakman/aoc/lib"
+)
+
+func First_star(input string) string {
+	println(input)
+	return "Not implemented yet"
+}
+
+func main() {
+	println(First_star(lib.Get_Input(2023, 1)))
+}
--- go/lib/misc.go
+++ go/lib/misc.go
@@ -1,7 +1,12 @@
 package lib
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 )
 
@@ -20,4 +25,58 @@ func Ok[T any](value T, err error) T {
 		panic(err)
 	}
 	return value
+}
+
+func Tmpdir() string {
+	cwd := Ok(os.Getwd())
+	path := filepath.Join(cwd, "../tmp")
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		panic(err)
+	}
+	return path
+}
+
+func SessionCookie() string {
+	path := filepath.Join(Tmpdir(), "sessionCookie.txt")
+	bytes := Ok(os.ReadFile(path))
+	return string(bytes)
+}
+
+func download_input(year int, day int) []byte {
+	url := fmt.Sprintf("https://www.adventofcode.com/%d/day/%d/input", year, day)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.AddCookie(&http.Cookie{Name: "session", Value: SessionCookie()})
+
+	client := &http.Client{}
+	resp, err := client.Do(req) // TODO: throws 400 Bad Request for some reason
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		err = errors.New(url +
+			"\nresp.StatusCode: " + strconv.Itoa(resp.StatusCode))
+		panic(err)
+	}
+
+	return Ok(ioutil.ReadAll(resp.Body))
+}
+
+func Get_Input(year int, day int) string {
+	path := filepath.Join(Tmpdir(), fmt.Sprintf("%d-%d-input.txt", year, day))
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		bytes := download_input(year, day)
+		err = os.WriteFile(path, bytes, 0777)
+		if err != nil {
+			panic(err)
+		}
+		return string(bytes)
+	}
+	return string(Ok(os.ReadFile(path)))
 }
