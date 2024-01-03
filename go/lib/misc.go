0017c5264594771e10bd081b13ec89e20e8a0ccf
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-01-04 07:07:24 +1100

    Added `Ok`

--- go/lib/misc.go
+++ go/lib/misc.go
@@ -1,8 +1,8 @@
 package lib
 
 import (
-	"strconv"
 	"fmt"
+	"strconv"
 )
 
 // Parses a string into an int, panicking if it fails.
@@ -14,3 +14,10 @@ func ParseInt(s string) int {
 	return i
 }
 
+// Generic function that takes a tuple and returns the value, else panics if `err != nil`.
+func Ok[T any](value T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
