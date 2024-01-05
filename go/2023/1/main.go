de373f4843e5f26725dddb6ee98812031ae26542
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-01-05 14:07:12 +1100

    Completed 2023/1

--- go/2023/1/main.go
+++ go/2023/1/main.go
@@ -2,13 +2,42 @@ package main
 
 import (
 	"danieloakman/aoc/lib"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-func First_star(input string) string {
-	println(input)
-	return "Not implemented yet"
+func isNumber(str string) bool {
+	_, err := strconv.Atoi(str)
+	return err == nil
+}
+
+func parseLine(str string) int {
+	first := ""
+	last := ""
+	for i := range str {
+		char := string(str[i])
+		if isNumber(char) {
+			if first == "" {
+				first = char
+				last = char
+			} else {
+				last = char
+			}
+		}
+	}
+	return lib.Ok(strconv.Atoi(first + last))
+}
+
+func FirstStar(input string) string {
+	sum := 0
+	lines := strings.Split(input, "\n")
+	for i := range lines {
+		sum += parseLine(lines[i])
+	}
+	return fmt.Sprint(sum)
 }
 
 func main() {
-	println(First_star(lib.Get_Input(2023, 1)))
+	println(FirstStar(lib.GetInput(2023, 1)))
 }
