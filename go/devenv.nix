5e19a0391103ef6c8887485d6bf2545ae5f0c670
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-01-17 14:26:52 +1100

    Added a devenv

---
+++ go/devenv.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+
+  ];
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    export GH_TOKEN=$(pass github_tokens/personal)
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+  languages.go = {
+    enable = true;
+    # version = "1.21.5";
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
