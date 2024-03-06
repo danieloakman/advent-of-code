e769df23743de89ccd6def17242017ecd3c441d3
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-03-06 17:00:16 +1100

    Converted day1 2017 to solution

--- typescript/src/years/2017/day1.ts
+++ typescript/src/years/2017/day1.ts
@@ -1,20 +1,22 @@
 // https://adventofcode.com/2017/day/1
 // https://adventofcode.com/2017/day/1/input
 
-import { readFileSync } from 'fs';
-import { subStrings } from '../../lib/utils';
+import { iter } from 'iteragain-es';
+import { Solution, subStrings, add } from '../../lib';
 const last = require('lodash/last');
 
-const input = readFileSync(__filename.replace('.ts', '-input'), 'utf-8');
+export const solution = new Solution(2017, 1)
+  .firstStar(async input => {
+    let num = input[0] === last(input) ? Number(input[0]) : 0;
+    for (const pair of subStrings(input, 2, true)) if (pair[0] === pair[1]) num += Number(pair[0]);
+    return num;
+  })
+  .secondStar(async input => {
+    return iter(pairs(input)).reduce(add);
+  })
+  .main(import.meta.path);
 
-// First Star:
-let num = input[0] === last(input) ? Number(input[0]) : 0;
-for (const pair of subStrings(input, 2, true)) if (pair[0] === pair[1]) num += Number(pair[0]);
-console.log({ num });
-
-// Second Star:
-function* pairs(input) {
+function* pairs(input: string) {
   const steps = input.length / 2;
   for (let i = 0; i < input.length; i++) if (input[i] === input[(i + steps) % input.length]) yield Number(input[i]);
 }
-console.log({ num: [...pairs(input)].reduce((a, b) => a + b) });
