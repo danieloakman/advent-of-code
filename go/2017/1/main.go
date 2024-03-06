1e0930012d72a88ee09be0f07aba1b4fefd168ae
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-03-06 17:17:36 +1100

    first stat done for golang 2017 1

--- go/2017/1/main.go
+++ go/2017/1/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	// "danieloakman/aoc/lib/iter"
+	"danieloakman/aoc/lib/iter"
 	"danieloakman/aoc/lib"
+	"strconv"
 )
 
+func firstStar(input string) int {
+	// lines := strings.Split(input, "\n")
+	num := 0
+	if input[len(input)-1] == input[0] {
+		num = lib.Ok(strconv.Atoi(string(input[0])))
+	}
+	for a, b := range iter.Pairwise(iter.FromString(input)) {
+		if a == b {
+			num += lib.Ok(strconv.Atoi(string(a)))
+		}
+	}
+	return num
+}
+
 func main() {
-	input := lib.GetInput(2015, 1)
-	// println(input)
-	panic("Not implemented")
+	input := lib.GetInput(2017, 1)
+	println("firstLine", firstStar(input))
 }
