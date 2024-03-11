a899d4196076a1c102152f91232d690bc22db2d2
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2024-03-11 14:36:56 +1100

    Changes to 2023/3

--- go/2023/3/main.go
+++ go/2023/3/main.go
@@ -7,10 +7,18 @@ import (
 
 func firstStar(input string) string {
 	arr := ndarray.NewInt([]int{5, 5})
-	println(arr.Get(4, 4))
-	arr.Set(2, 0, 0)
-	println(arr.Get(4, 4))
-	println(arr.Values().ToSlice())
+	// println(arr.Get(4, 4))
+	arr.Set(1, 1, 0)
+	arr.Set(3, 3, 0)
+	// println(arr.Get(4, 4))
+	arr.Iter().FilterMap(func (pv ndarray.PointValue[int]) *ndarray.PointValue[int] {
+		if pv.Value == 0 {
+			return nil
+		}
+		return &pv
+	}).Each(func (v ndarray.PointValue[int]) {
+		println(v.Value)
+	})
 	return "TODO"
 }
 
